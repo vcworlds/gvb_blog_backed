75195b618410a8b2a1ecdc50318ff2bb79a558a3
vcworlds <2428537532@qq.com>
2024-07-10 14:23:23 +0800

    统一响应封装

--- api/settings_api/settings_info.go
+++ api/settings_api/settings_info.go
@@ -2,9 +2,10 @@ package settings_api
 
 import (
 	"github.com/gin-gonic/gin"
+	"gvb_blog/response"
 	"net/http"
 )
 
 func (SettingsApi) SettingsInfoView(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"msg": "你好gin"})
+	response.Result(ctx, http.StatusOK, 200, "获取成功", gin.H{"id": 1})
 }
