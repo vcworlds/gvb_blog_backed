86e25591bf94ab41cdea16abdaa22ec241b956c8
vcworlds <2428537532@qq.com>
2024-07-12 17:34:34 +0800

    补充配置settings

--- config/enter.go
+++ config/enter.go
@@ -5,4 +5,8 @@ type Config struct {
 	Logger   Logger   `yaml:"logger"`
 	System   System   `yaml:"system"`
 	SiteInfo SiteInfo `yaml:"site_info"`
+	QQ       QQ       `canGet:"qq" yaml:"qq"`
+	Email    Email    `canGet:"email" yaml:"email"`
+	Jwt      Jwt      `canGet:"jwt" yaml:"jwt"`
+	QiNiu    QiNiu    `canGet:"qiNiu" yaml:"qi_niu"`
 }
